dae2ff5284a4ce9b0c148c0b076b5a638d70469c
OksanaQA5 <137555719+OksanaQA5@users.noreply.github.com>
2023-07-06 13:38:25 +0300

    HW3

--- StringMethods/StringMethods2.java
+++ StringMethods/StringMethods2.java
@@ -1,21 +1,23 @@
 package StringMethods;
 
-public class StringMethods2 {
-    public static void main(String[] args) {
-        String string = "Testing, is my favourite job";
-        String[] words = string.split(" ");
+public static void main(String[] args) {
+            String string = "Testing, is my favourite job";
+            String word1 = string.split("\\s+")[0];
+            word1 = word1.replace(",", "");
+            String word2 = string.split("\\s")[1];
+            String word3 = string.split("\\s")[2];
+            String word4 = string.split("\\s")[3];
+            String word5 = string.split("\\s")[4];
+            System.out.println("Cлово1 = " + word1 + ", Длина этого слова = " + word1.length() + ".");
+            System.out.println("Cлово2 = " + word2 + ", Длина этого слова = " + word2.length() + ".");
+            System.out.println("Cлово3 = " + word3 + ", Длина этого слова = " + word3.length() + ".");
+            System.out.println("Cлово4 = " + word4 + ", Длина этого слова = " + word4.length() + ".");
+            System.out.println("Cлово5 = " + word5 + ", Длина этого слова = " + word5.length() + ".");
 
-        boolean isFirstWordLonger = true;
-        int firstWordLength = words[0].length();
-
-        for (int i = 0; i < words.length; i++) {
-            System.out.println("Cлово" + (i + 1) + " = " + words[i] + ", Длина этого слова = " + words[i].length());
-
-            if (i > 0 && words[i].length() > firstWordLength) {
-                isFirstWordLonger = false;
-            }
+            boolean result = word1.length() > word2.length() &&
+                    word1.length() > word3.length() &&
+                    word1.length() > word4.length() &&
+                    word1.length() > word5.length();
+            System.out.println(result);
         }
-
-        System.out.println("Первое слово длиннее остальных: " + isFirstWordLonger);
-    }
 }
